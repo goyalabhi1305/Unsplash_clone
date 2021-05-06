437d07cfb6261c50c7c9dc4880e956eaa0161a19
Abhishek Goyal <goyalabhi1305@gmail.com>
2021-05-06 12:25:59 +0530

    added the api from unsplash clone

--- src/Images.js
+++ src/Images.js
@@ -1,32 +1,47 @@
 import React, { useEffect, useState } from 'react'
-import axios from './axios'
+import axios from 'axios'
+
 
 const API_KEY = "ZMJmZPc1yC4VwB0jluonDVPAtZE92VwNhX8iOq2eZ2k";
-const url_fetch = `/photos?page=1&client_id=${API_KEY}`;
+const url_fetch = `https://api.unsplash.com/photos?page=1&client_id=${API_KEY}`;
 
 
 
 function Images() {
 
-    // const [img, SetImg] = useState([]);
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const request = await axios.get(url_fetch);
-            
-    //         SetImg(request.data.results[
-                
-    //             Math.floor(Math.random() * request.data.results.length - 1)
-    //         ]);
-    //         return request;
-    // }
-    // fetchData();
-    // }, [])
 
-    
+
+    const [img, SetImg] = useState([]);
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const request = await axios.get(url_fetch);
+                request.data?.map((val) => {
+                    const img_url = val.urls.regular
+                    SetImg(state=> [...state,img_url])
+                })
+            }
+            catch(e){
+                console.log("an err occured. Error: ",e)
+            }
+    }
+    fetchData();
+    }, [])
     return (
-        <div>
+        <>
+            <div className="imgcnt">
+
+                {img?.map((val) => {
+                    return (
+                <img src={val} alt="" className="img_singl" />
+                        
+                    )
+                    
+                })}
             
-        </div>
+            </div>
+        </>
     )
 }
 
